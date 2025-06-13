a9cb7c1d7007287072cdb9fa3f7821a01cb9ee7b
Bagate11 <ermias03555@gmail.com>
2025-06-13 12:32:34 +0200

    Update Program.cs

--- Systemhantering/Program.cs
+++ Systemhantering/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Systemhantering.DBcontext;
+
 namespace Systemhantering
 {
     public class Program
@@ -13,6 +16,9 @@ namespace Systemhantering
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<UserAuthContext>(options =>
+            options.UseSqlServer("Server=localhost;Database=UserAuthDB;Trusted_Connection=True;TrustServerCertificate=True"));
 
             var app = builder.Build();
 
